041aa2f94af38ca19eeed48bb309e3276e89cb8d
Akshpatil1530 <77833577+Akshpatil1530@users.noreply.github.com>
2021-02-07 14:45:52 +0530

    Update using_push.js

--- using_push.js
+++ using_push.js
@@ -2,13 +2,13 @@
 class live_data_structure
 {
 	constructor (...args)
-    {
-    	this.ds = []
-    	for(var i =0; i< args.length; i++)
     	{
-    		this.ds.push(args[i])
+    		this.ds = []
+    		for(var i =0; i< args.length; i++)
+    		{
+    			this.ds.push(args[i])
+    		}
     	}
-    }
 }
 let p = new live_data_structure("jvt", "Aksh", "patil", 100, 200)
 document.write(p.ds)
@@ -18,11 +18,11 @@ document.write(p.ds)
 class corporate_information
 {
 	constructor (...args)
-    {
-    	this.data = []
-        for(var i =0; i< args.length; i++)
     	{
-    		this.data.push(args[i])
+    		this.data = []
+        	for(var i =0; i< args.length; i++)
+    		{
+    			this.data.push(args[i])
     	}
     }
 }
@@ -33,11 +33,11 @@ document.write(company.data)
 class personal_details
 {
 	constructor (...args)
-    {
-    	this.details = []
-        for(var i =0; i< args.length; i++)
     	{
-    		this.details.push(args[i])
+    		this.details = []
+        	for(var i =0; i< args.length; i++)
+    		{
+    			this.details.push(args[i])
     	}
     }
 }
@@ -48,12 +48,12 @@ document.write(person.details)
 class network_connection
 {
 	constructor (...args)
-    {
-    	this.data = []
-        for(var i =0; i< args.length; i++)
     	{
-    		this.data.push(args[i])
-    	}
+    		this.data = []
+        	for(var i =0; i< args.length; i++)
+    		{
+    			this.data.push(args[i])
+    		}
     }
 }
 let network = new network_connection(1135497, 11128550, "10.245.97.123")
@@ -63,12 +63,12 @@ document.write(network.data
 class network_usage
 {
 	constructor (...args)
-    {
-    	this.data = []
-        for(var i =0; i< args.length; i++)
     	{
-    		this.data.push(args[i])
-    	}
+    		this.data = []
+        	for(var i =0; i< args.length; i++)
+    		{
+    			this.data.push(args[i])
+    		}
     }
 }
 let network = new network_usage("21.26 GB", "30.05 mbps", "32.06 mbps")
