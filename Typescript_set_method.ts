e0756e3e2a31fd3d76992655eadbaf3070723cc1
Akshpatil1530 <77833577+Akshpatil1530@users.noreply.github.com>
2021-02-12 23:13:04 +0530

    Update Typescript_set_method.ts

--- Typescript_set_method.ts
+++ Typescript_set_method.ts
@@ -1,25 +1,30 @@
-class ContactDetails
+class Contact_Details
 {
     phone_no: number;
     email_id: string;
     address: string;
-    constructor(phone_no: number, mail: string, address: string){
+    constructor(phone_no: number, mail: string, address: string)
+    {
         this.phone_no = phone_no
         this.email_id = mail
         this.address = address
     }
-    display(){
+    display()
+    {
         console.log(this.phone_no, this.email_id, this.address)
     }
-    set_phone_no(new_phone_no: number){
+    set_phone_no(new_phone_no: number)
+    {
         this.phone_no = new_phone_no
     }
-    set_mail(new_mail: string){
+    set_mail(new_mail: string)
+    {
         this.email_id = new_mail
     }
-    set_address(new_address: string){
+    set_address(new_address: string)
+    {
         this.address = new_address
     }
 }
-var mycontactdetails = new ContactDetails(1234567890, "abc@mail.com", "#03, Btm layout, Banglore")
-mycontactdetails.display()
+var my_contact_details = new Contact_Details(987654321, "akshata@mail.com", "#03, Btm layout, Banglore")
+my_contact_details.display()
